a7c0c5b7fe6780a54bcd34d528fa39f52737ff99
Rohan Tarai <rohantarai@users.noreply.github.com>
2017-07-19 02:25:38 +0530

    added CDN for select2, modified grecaptcha site key, added contactus modal and done minor changes

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -6,16 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Soacial</title>
+    <title>SOAcial</title>
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('soacial.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/imageviewer/0.5.1/viewer.min.css">
     <link rel="stylesheet" href="{{ asset('/css/jquery.guillotine.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/notify.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/select2.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/imageviewer/0.5.1/viewer.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css">
     <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
     {{--<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">--}}
     {{--<link rel="stylesheet" href="{{ asset('/css/font-awesome.min.css') }}">--}}
@@ -23,6 +22,7 @@
     {{--<link rel="stylesheet" href="{{ asset('/css/loading.css') }}">--}}
     {{--<link rel="stylesheet" href="{{ asset('css/bootstrap-multiselect.css') }}">--}}
     {{--<link rel="stylesheet" href="{{ asset('/css/bootstrap-tagsinput.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('/css/select2.min.css') }}">--}}
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -84,7 +84,10 @@
 {{--@if(!Auth::check())
 <nav class="navbar navbar-default">
     <div class="container">
-        <div class="navbar-header"><a class="navbar-brand navbar-link" href="{{ url('/') }}">SOACIAL</a>
+        <div class="navbar-header">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('img/logo2.png') }}" alt="Brand" style="margin-top: -15px; max-width: 50px;">
+        </a>
             <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
         </div>
         <div class="collapse navbar-collapse" id="navcol-1">
@@ -104,7 +107,7 @@
     var onloadCallback = function() {
         $( ".g-recaptcha" ).each(function() {
             grecaptcha.render($( this ).attr('id'), {
-                'sitekey' : "6LduXygUAAAAAJyTU953xLIxsIgpH76eOEHscoFX"
+                'sitekey' : "6LcI4SgUAAAAADpITy9vbUJADnJ6vAkofQKDK1WN"
             });
         });
     };
@@ -115,13 +118,15 @@
         <ul class="list-inline">
             <li role="button" data-toggle="modal" data-target="#terms"><a href=""></a>Terms</li>
             <li role="button" data-toggle="modal" data-target="#privacy"><a href=""></a>Privacy</li>
-            <li role="button" data-toggle="modal" data-target="#contact"><a href=""></a>Contact</li>
+            <li role="button" data-toggle="modal" data-target="#contactUs"><a href=""></a>Contact</li>
             <li role="button" data-toggle="modal" data-target="#about"><a href=""></a>About</li>
         </ul>
         <ul class="list-inline">
-            <li>Soacial &copy; 2017-18</li>
+            <li>SOAcial &copy; 2017</li>
         </ul>
     </div>
+
+    <!-- Terms and Conditions -->
     <div class="modal fade" tabindex="-1" role="dialog" id="terms">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -142,11 +147,54 @@
                     </ul>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="border-radius: 50px;">Close</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal" style="border-radius: 50px;">Close</button>
                 </div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
+    <!-- Contact Us -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="contactUs">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form id="contactForm" method="post" role="form" action="{{ route('contactUs') }}">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Contact Us</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="senderName" class="control-label">Full Name:</label>
+                            <input type="text" name="senderName" class="form-control input-sm" id="senderName" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="senderEmail" class="control-label">Email id:</label>
+                            <input type="text" name="senderEmail" class="form-control input-sm" id="senderEmail" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject" class="control-label">Subject:</label>
+                            <input type="text" name="subject" class="form-control input-sm" id="subject" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="messages" class="control-label">Message:</label>
+                            <textarea name="messages" class="form-control input-sm" id="messages" required></textarea>
+                        </div>
+                        <input type="hidden" value="{{ csrf_token() }}" name="_token">
+                        <div id="recaptcha3" class="g-recaptcha"></div>
+                        <img src="{{ asset('/img/ajax-loader.gif') }}" id="contactForm-loading-indicator" style="display:none">
+                        <div id="contactform-alert">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" id="contactFormSubmitBtn" style="border-radius: 50px;">Send</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal" style="border-radius: 50px;">Close</button>
+                    </div>
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+    <!-- Privacy Policy -->
     <div class="modal fade" tabindex="-1" role="dialog" id="privacy">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -163,50 +211,13 @@
                     </ul>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="border-radius: 50px;">Close</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal" style="border-radius: 50px;">Close</button>
                 </div>
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-    <div class="modal fade" tabindex="-1" role="dialog" id="contact">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <form id="contactForm" method="post" role="form" action="{{ route('contactUs') }}">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Contact Us</h4>
-                    </div>
-                    <div class="modal-body">
-                            <div class="form-group">
-                                <label for="senderName" class="control-label">Full Name:</label>
-                                <input type="text" name="senderName" class="form-control input-sm" id="senderName" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="senderEmail" class="control-label">Email id:</label>
-                                <input type="text" name="senderEmail" class="form-control input-sm" id="senderEmail" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="subject" class="control-label">Subject:</label>
-                                <input type="text" name="subject" class="form-control input-sm" id="subject" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="messages" class="control-label">Message:</label>
-                                <textarea name="messages" class="form-control input-sm" id="messages" required></textarea>
-                            </div>
-                            <input type="hidden" value="{{ csrf_token() }}" name="_token">
-                            <div id="recaptcha3" class="g-recaptcha"></div>
-                            <img src="{{ asset('/img/ajax-loader.gif') }}" id="contactForm-loading-indicator" style="display:none">
-                            <div id="contactform-alert">
-                            </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary btn-sm" id="contactFormSubmitBtn" style="border-radius: 50px;">Send</button>
-                        <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="border-radius: 50px;">Close</button>
-                    </div>
-                </form>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
+
+    <!-- About Us -->
     <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" id="about">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
@@ -215,7 +226,7 @@
                     <h4 class="modal-title">About Us</h4>
                 </div>
                 <div class="modal-body">
-                    <strong>Soacial</strong>, a social network platform for the students of SOA University, Bhubaneswar.
+                    <strong>SOAcial</strong>, a social network platform for the students of SOA University, Bhubaneswar.
                     Here you can search anyone from any institute of any programme of any year and of any interests.
                     <br>
                     Siksha 'O' Anusandhan University has 9 institutes namely:
@@ -232,7 +243,7 @@
                     </ol>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="border-radius: 50px;">Close</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal" style="border-radius: 50px;">Close</button>
                 </div>
             </div>
         </div>
